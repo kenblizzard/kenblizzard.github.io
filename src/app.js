171a703bb69b95504c24789bc5c778a0d09e282e
kenblizzard <kenneth.bolico@gmail.com>
2015-09-19 00:03:28 +0800

    added animations

--- src/app.js
+++ src/app.js
@@ -1,37 +1,48 @@
-angular.module('resume', ['ngRoute'])
-    .config(['$routeProvider', function ($routeProvider) {
+"use strict";
+
+angular.module('resume', ['ngRoute', 'ngAnimate'])
+    .config(['$routeProvider', function ($routeProvider, $locationProvider) {
         $routeProvider.when('/', {
             templateUrl: "src/views/welcome.html",
         })
         .when('/me', {
             templateUrl: "src/views/main.html",
             controller: "MainController"
+        })
+        .when('/connect', {
+            templateUrl: "src/views/connect.html"
+        })
+        .when('/works', {
+            templateUrl: "src/views/works.html"
         });
-    }])
-	.factory('ResumeDetailsFactory', function() {
 
-		var resumeDetails = {
-			name: "Kenneth J. Bolico",
-			address: "340 Lakas St. Bacood, Sta. Mesa, Manila",
-			cellphoneNum: "09330726232",
-			emailAddress: "kenneth.bolico@gmail.com",
-			facebook: "facebook.com/kenneth.bolico",
-			github: "github.com/kenblizzard",
-			objective: "I am a Web Developer with passion in creating website using Javascript technologies and is willing to learn alot more to become a talented Full Stack Web Developer in the near future. Also, I love codes and pizzas.",
-			education: {
-				school: "Polytechnic University of the Philippines",
-				location: "Sta. Mesa, Manila",
-				course: "BS Computer Science",
-				year: "2011 - 2015"
-			},
-			technologies: {				
-				webDev: ["HTML5","CSS","Javascript","NodeJs","JQuery", "AngularJS", "BackboneJS", "Bootstrap", "momentjs", "Handlebars", "Faye", "Git"],
-				packageManagers: ["NPM", "Bower"],
-				taskRunner: ["Grunt"],
-				withBackground: ["Java", "C_Sharp"],
-				database: ["MongoDB", "PostgresSQL", "MySQL"]
-			},
-			experience: [
+        $locationProvider.html5Mode(true);
+
+    }])
+	.factory('ResumeDetailsFactory', function () {
+
+	    var resumeDetails = {
+	        name: "Kenneth J. Bolico",
+	        address: "Manila",
+	        cellphoneNum: "09330726232",
+	        emailAddress: "kenneth.bolico@gmail.com",
+	        facebook: "facebook.com/kenneth.bolico",
+	        github: "github.com/kenblizzard",
+	        objective: "I am a Web Developer with passion in creating website using Javascript technologies and is willing to learn alot more to become a talented Full Stack Web Developer in the near future. Also, I love codes and pizzas.",
+	        education: {
+	            school: "Polytechnic University of the Philippines",
+	            location: "Sta. Mesa, Manila",
+	            course: "BS Computer Science",
+	            year: "2011 - 2015"
+	        },
+	        technologies: {
+	            webDev: ["HTML5", "CSS", "Javascript", "NodeJs", "JQuery", "AngularJS", "BackboneJS", "Bootstrap", "momentjs", "Handlebars", "Faye", "Git"],
+	            packageManagers: ["NPM", "Bower"],
+	            taskRunner: ["Grunt"],
+	            withBackground: ["Java", "C_Sharp"],
+	            database: ["MongoDB", "PostgresSQL", "MySQL"]
+	        },
+	        experience: [
                 {
                     employer: "Pinoy Programmers League - Non-profit Organization (Member)",
                     startDate: new Date(2015, 7, 14),
@@ -40,59 +51,60 @@ angular.module('resume', ['ngRoute'])
                         name: "PPL Main Site",
                         url: "http://main.pinoyprogleague.eu.org",
                         description: "The home site of the organization",
-                        technologies: ["AngularJS","Bootstrap", "HTML5", "CSS","Javascript"],
+                        technologies: ["AngularJS", "Bootstrap", "HTML5", "CSS", "Javascript"],
                         contributions: [
                             "Implemented in html/css/javascript the web design made by our Graphics Designer",
                             "Implemented the site using AngularJS Framework",
                             "Implemented the responsive view using Bootstrap"
-                            ]
+                        ]
                     }]
                 },
 				{
-					employer: "Seansoft Corporation",
-					startDate: new Date(2015,3, 1),
-					endDate: "Present",					
-					projects: [{
-						name: "Scrumonline",
-						url: "http://node.Scrumonline.net",
-						description: "Scrum tool for tracking of projects' status and tasks management with additional features such as time entry system, and payroll system.",
-						technologies: ["NodeJs","Faye", "MongoDB", "BackboneJS","JQuery", "Bootstrap", "Handlebars", "momentjs"],
-						contributions: [
+				    employer: "Seansoft Corporation",
+				    startDate: new Date(2015, 3, 1),
+				    endDate: "Present",
+				    projects: [{
+				        name: "Scrumonline",
+				        url: "http://node.Scrumonline.net",
+				        description: "Scrum tool for tracking of projects' status and tasks management with additional features such as time entry system, and payroll system.",
+				        technologies: ["NodeJs", "Faye", "MongoDB", "BackboneJS", "JQuery", "Bootstrap", "Handlebars", "momentjs"],
+				        contributions: [
 							"Implemented the real time updates of views of different client browsers whenever fields were modified by a user. Used 'Faye messaging' as communication tool between client and server.",
 							"Implemented the upload and download of file attachments using AJAX, and MongoDB GridFS",
 							"Setup 'Strider CI', a Continous Integration system for automated build and testing of the repository.",
 							"Implemented automated email notification using 'nodemailer.js'.",
 							"Developed the front-end using BackboneJS, JQuery, handlebars and Bootstrap."
-						]
-					}]
+				        ]
+				    }]
 				},
 				{
-					employer: "Devkinetics - Internship",
-					startDate: new Date(2014, 2, 31),
-					endDate: new Date(2014, 5, 1),
-					projects: [{
-						name: "ebayanihan.org",										
-						technologies: ["Java", "AngularJS","Bootstrap", "PostgresSQL"],
-						description: "An online portal where users can post projects (e.g. Feeding Program) to look for sponsors and also post the records such as budgets and expenses for transparency.",
-						contributions: [
+				    employer: "Devkinetics - Internship",
+				    startDate: new Date(2014, 2, 31),
+				    endDate: new Date(2014, 5, 1),
+				    projects: [{
+				        name: "ebayanihan.org",
+				        technologies: ["Java", "AngularJS", "Bootstrap", "PostgresSQL"],
+				        description: "An online portal where users can post projects (e.g. Feeding Program) to look for sponsors and also post the records such as budgets and expenses for transparency.",
+				        contributions: [
 							"Implemented a RESTful website using Java EE for server-side and AngularJS for the front-end"
-						]
-					}]
+				        ]
+				    }]
 
 				}
-			]
+	        ]
 
 
-		}
+	    }
 
 
 
-		return {
-			getDetails: function () {
-				return resumeDetails;
-			}
-		}
+	    return {
+	        getDetails: function () {
+	            return resumeDetails;
+	        }
+	    }
 	})
-	.controller('MainController', ['$scope', 'ResumeDetailsFactory', function($scope, ResumeDetailsFactory) {
-		$scope.resumeDetails = ResumeDetailsFactory.getDetails();
+	.controller('MainController', ['$scope', 'ResumeDetailsFactory', function ($scope, ResumeDetailsFactory) {
+	    $scope.resumeDetails = ResumeDetailsFactory.getDetails();
+	    $scope.pageClass = "page-home";
 	}]);
